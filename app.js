4767f2bf13d202f111a4f5534fddb98ac601b23e
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-03 16:43:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var fs = require("fs");
+fs.writeFileSync(
+  "testfile.txt",
+  "This is sample file create with help of writeFileSync"
+);
---
+++ basic.js
@@ -0,0 +1,14 @@
+const { username } = require("./data");
+
+console.log(username);
+
+// const { log } = require("console");
+
+// var a = 1;
+// while (a <= 5) {
+//   console.log("print a:-", a);
+//   a++;
+// }
+
+// var users = ["Sagar", "Mayur", "Rajesh", "Omkar", "Soham"];
+// console.log(users[3]);

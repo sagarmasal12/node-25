2a6e1de5c6e006d8dfabe0331a5bfd0185f92677
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-10 09:25:47 +0530

    cat me soft

--- command-line-input.js
+++ command-line-input.js
@@ -1,2 +1,14 @@
+// const arg = process.argv;
+// console.log("-----------", arg);
+
+const http = require("http");
 const arg = process.argv;
-console.log("-----------", arg);
+const port = arg[2];
+
+http
+  .createServer((req, res) => {
+    res.write("<h1>Testing the Port ...</h1>");
+    res.write("<h2>this is test page</h2>");
+    res.end();
+  })
+  .listen(port);

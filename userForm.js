9ec32f469e72d04f4d834344218882a42f538466
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-16 11:47:01 +0530

    export

--- userForm.js
+++ userForm.js
@@ -1,5 +1,9 @@
 function userForm(req,res){
-
-    res.end("userform function displayed")
+    res.write( `<form action="/submit" method= "post">
+        <input type="text" name="Name" placeholder="Enter Name">
+        <input type="text" name="email" placeholder="Enter Email">
+        <button>Submit</button>
+        </form>`
+    )
 }
 module.exports= userForm;

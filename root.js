e974c7c529d67a52ef6ac2dbe3309b2b1f2d81d0
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-16 10:31:26 +0530

    root file

--- root.js
+++ root.js
@@ -1,7 +1,8 @@
 const http = require("http");
+const userForm = require('./userForm')
 
 http.createServer((req,res)=>{
-    res.write("Hello Node")
+    userForm(req,res);
     res.end()
 }).listen(4800,()=>{
     console.log("Port Run:- http://localhost:4800")

3be6ea9761384ab33e7184f4e676f2a177f6a4f7
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-17 11:08:29 +0530

    asf

--- fscrud.js
+++ fscrud.js
@@ -7,7 +7,10 @@ fs.unlinkSync("textfile/banana.txt")
 fs.writeFileSync("textfile/banana.txt","This is a Fruit")
 fs.unlinkSync("textfile/banana.txt")
 
+
+console.log(process.argv[2])
 const data = fs.readFileSync("textfile/apple.txt","utf-8")
 
-console.log(data);
+fs.appendFileSync("textfile/apple.txt","and this is good for health")
 
+console.log(data);
--- server.js
+++ server.js
@@ -1,53 +1,22 @@
-// const http = require("http");
-// http
-//   .createServer((req, resp) => {
-//     resp.write("server created");
-//     resp.write("<h1>hello node</h1>");
-//     resp.end("end the server");
-//   })
-//   .listen(4700);
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
 
-// const http = require("http");
+app.use(cors());
+app.use(express.json())
 
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.write("server created<br>");
-//     res.write("<h1>Hello</h1>");
-//     res.end("end the server");
-//   })
-//   .listen(4800, () => {
-//     console.log("Server running on http://localhost:4800");
-//   });
 
-const { log } = require("console");
-const math = require("./math");
+// sample api 
+app.get("/api/users",(req,res)=>{
+  const users =[
+     { id: 1, name: "Sagar", role: "Developer" },
+    { id: 2, name: "Amit", role: "Tester" },
+  ]
+  res.json(users)
+})
 
-console.log("Addition:-", math.addn(45, 60));
-console.log("Substraction:-", math.subn(45, 12));
-
-console.log("Multiplication", math.mul(4, 5));
-console.log("Division", math.div(6, 2));
-
-const http = require("http");
-const { red } = require("colors");
-http
-  .createServer((req, resp) => {
-    resp.write(
-      "<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quisquam fugit, odio rerum inventore officia voluptate temporibus facere quam veritatis iure voluptatibus. Incidunt, saepe cum? Cum ut ea consectetur doloreFacere, quia unde. Voluptatibus vero dolorum placeat ipsa doloremque accusantium explicabo, ab eligendi, mollitia, quam blanditiis rem molestiae officia. Quis ea omnis totam provident. Laudantium illum ab quis id accusamus! Quia odit dolorem, veritatis molestiae, illum magni quisquam atque dolorum rerum ut blanditiis perspiciatis distinctio animi harum similique repellendus tempore rem iusto. Dolores exercitationem quas repellendus perferendis itaque maxime consectetur. Iure hic porro eum, tempora deleniti quia possimus ut unde eos sequi reprehenderit commodi placeat, quidem dignissimos reiciendis officia deserunt iusto dolor? Odio vel nulla ratione repudiandae, tempora voluptatem ut? Laborum fuga veritatis ex voluptas et eaque quia, excepturi distinctio aliquid nam magni quisquam molestiae sunt reprehenderit, expedita aspernatur beatae delectus ipsa quae </p>"
-    );
-
-    resp.write("<h1>Today's Topic is - Nodemon, Server and Basic Node</h1>");
-    resp.end("");
-  })
-  .listen(4500);
-
-http
-  .createServer((req, resp) => {
-    // resp.setHeader("Content-Type");
-    // resp.setHeader("Content-Type", "text/html");
-    resp.render("<h2>Other Server</h2>");
-    resp.end("");
-  })
-
-  .listen(4100);
+app.listen(PORT,()=>{
+  console.log(`server running at http://localhost:${PORT}`);
+  
+});
